11cd905b90390bef0c685d876c6c811b769e9425
Ruben Maurer <rubenmaurer@live.de>
2022-11-04 12:47:53 +0100

    add missing binominal

--- src/BerryCost.jl
+++ src/BerryCost.jl
@@ -16,6 +16,28 @@ function getcosts(xh, xd, t)
             diff = ch - cd)
 end
 
+function quorum(tp, fp)
+    # P(fp) + (P(tp) - P(fp) / 2)
+    return fp + ((tp - fp) / 2)
+end
+
+function calculateaverage(input::Vector{Vector{Float64}}, t::Float64)
+    tp = []
+    fp = []
+    for x::Vector{Float64} in input
+        p = sum(pdf.(mix, x); init = 0) / length(x)
+
+        m = mean(x)
+        if m > t
+            push!(tp, (p = p, n = length(x)))
+        else
+            push!(fp, (p = p, n = length(x)))
+        end
+    end
+
+    return (tp = tp, fp = fp)
+end
+
 # distributions
 xh = Normal(4.2, 5)
 xd = Normal(7, 4)
@@ -30,32 +52,33 @@ agents = [rand(mix, x) for x in 1:100]
 
 # calculate threshold
 costs = [getcosts(xh, xd, x) for x in min:.001:max]
-t = [x for x in costs if x.diff == maximum([p.diff for p in costs])][1]
 positives = [getpositives(xh, xd, t) for t in min:1:max]
+t = [x for x in costs if x.diff == maximum([p.diff for p in costs])][1]
 
-# P(fp) + (P(tp) - P(fp) / 2) - calculate quorum
-q = t.fp + ((t.tp - t.fp) / 2)
+# calculate quorum
+q = quorum(t.tp, t.fp)
 
 tpg = []
 fpg = []
 
 for x::Vector{Float64} in agents
-    #local tp = reduce(+, [pdf.(mix, p) for p::Float64 in x if p > t.t]; init = 0.0)
-    #local fp = reduce(+, [pdf.(mix, p) for p::Float64 in x if p < t.t]; init = 0.0)
 
-    local tp = [p for p::Float64 in x if p > t.t]
+    # calculate TPR and FPR
     local n = length(x)
+    fpb = Binomial(n, t.fp)
+    tpb = Binomial(n, t.tp)
 
-    if length(tp) > n * q
-        push!(tpg, (p = 1, n = n))
-    else
-        push!(fpg, (p = 0, n = n))
-    end
+    push!(tpg, tpb)
+    push!(fpg, fpb)
 end
+
+# calculate values for average
+avg = calculateaverage(agents, t.t)
 
 # plots
 theme(:dark)
 
+# distribution plot
 dplt = plot(xh, label = "Harmless", color = "green")
 plot!(dplt, xd, label = "Dangerous", color = "red")
 
@@ -64,18 +87,26 @@ xlabel!("Value")
 ylabel!("Density")
 vline!([t.t], label = "Threshold", color = "blue", line = (1, :dash), annotation = (t.t, 0.01, t.t))
 
-mixplt = scatter([(p.n, p.p) for p in tpg], seriestype = :scatter, label = "True positive", legend = true)
+# quorums plot
+bb = Binomial(100, .5)
+show(ccdf(bb, 0))
+
+mixplt = plot([ccdf(x, q) for x in tpg], seriestype = :scatter, label = "True positive", legend = true)
 #ylims!(0.0, .1)
 #xlims!(0, 100)
 #hline!([q], label = "Quorum", color = "blue", line = (1, :dash))
-plot!(mixplt, [(p.n, p.p) for p in fpg], seriestype = :scatter, label = "False positive")
+plot!(mixplt, [ccdf(x, q) for x in fpg], seriestype = :scatter, label = "False positive")
 
-mplot = scatter()
+# plot for average desicsion making
+mplot = scatter([(p.n, p.p) for p in avg.tp], label = "True postive")
+plot!(mplot, [(p.n, p.p) for p in avg.fp], seriestype = :scatter, label = "False positive")
 
+# roc plot
 roc = plot([(p.fp, p.tp) for p in positives], legend = false)
 title!("ROC")
 xlabel!("false positive")
 ylabel!("true positive")
 
-plot(dplt, mixplt, roc, layout = (3, 1))
-plot!(size=(800,600))
+# combined plots
+plot(dplt, mixplt, mplot, roc, layout = (4, 1))
+plot!(size=(1280,1280))
